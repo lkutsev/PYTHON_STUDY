79fcc28a3ca8b0c761d548ae1951e05a8391a8fc
LKutsev <2211@bk.ru>
2022-07-15 21:05:59 +0300

    task1

--- Lesson1/task1.py
+++ Lesson1/task1.py
@@ -1,2 +1,11 @@
-print("hello!")
+txt = str(input("Введи текст:"))
+print("текст: ", txt)
+_int = int(input("Введи целое число:"))
+print("Число:", _int)
+_bool = bool(input("Введи bool:"))
+print("Bool:", _bool)
+_float = float(input("Введи float:"))
+print("float:", _float)
+_list = list(input("Введи  list:"))
+print("list:", _list)
 
