65e03e6977c409b2e35fe0ebb4d5164858105e2f
LKutsev <2211@bk.ru>
2022-07-17 22:18:45 +0300

    task3

--- Lesson1/task3.py
+++ Lesson1/task3.py
@@ -1,1 +1,8 @@
-s = int(input('Введите количество секунд:'))
+s = int(input('Введите одноразрядное десятичное положительное число:'))
+res = s + 10*s + 100*s + 10*s+ s+ s
+print("Вариант 1: %d"%res)
+ss = str(s)
+res2 = int(ss) + int(ss+ss) + int(ss+ss+ss)
+print("Вариант 2: %d"%res2)
+res3 = 123*s
+print("Вариант 3: %d"%res3)
