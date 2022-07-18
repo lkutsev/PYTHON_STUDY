d5513a585f00db35d833e281e69f83f705eb010b
LKutsev <2211@bk.ru>
2022-07-19 02:02:54 +0300

    lesson2

--- Lesson2/task2.py
+++ Lesson2/task2.py
@@ -4,7 +4,7 @@
 # Для заполнения списка элементов нужно использовать функцию input().
 lst = list(input('введите текст (он же список):'))
 print(f'введен список {lst}')
-for i in range(0,len(lst),2):
-    if i+1<len(lst):
-        lst.insert(i,lst.pop(i+1))
+for i in range(0, len(lst), 2):
+    if i + 1 < len(lst):
+        lst.insert(i, lst.pop(i + 1))
 print(f'получен список {lst}')
